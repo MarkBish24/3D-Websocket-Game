1c6bf24742d366c2dfb6df233842d079d9151b21
MarkBish24 <markbish24@gmail.com>
2026-03-14 13:11:19 -0700

    created friendship tables in database

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -7,3 +7,28 @@ CREATE TABLE IF NOT EXISTS players (
   picture TEXT,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+--------------------------------------
+-------- FRIENDSHIP SYSTEM -----------
+--------------------------------------
+
+CREATE TABLE IF NOT EXISTS friendships (
+	id SERIAL PRIMARY KEY,
+	requester_id INT NOT NULL REFERENCES players(id) ON DELETE CASCADE,
+	addressee_id INT NOT NULL REFERENCES players(id) ON DELETE CASCADE,
+	status VARCHAR(20) NOT NULL DEFAULT 'pending',
+	blocked_by INT REFERENCES players(id),
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+	CONSTRAINT no_self_friendship CHECK (requester_id != addressee_id),
+	CONSTRAINT unique_friendship UNIQUE (requester_id, addressee_id)
+);
+
+CREATE TABLE IF NOT EXISTS friendship_events(
+	id SERIAL PRIMARY KEY,
+	friendship_id INT NOT NULL REFERENCES friendships(id) ON DELETE CASCADE,
+	actor_id INT NOT NULL REFERENCES players(id), -- who triggered this evenet
+	event VARCHAR(20) NOT NULL, -- 'requested', 'accepted', 'declined', 'blocked'
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)

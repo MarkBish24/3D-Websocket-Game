40a228262ef1357a543c5cef24396f6673ee7510
MarkBish24 <markbish24@gmail.com>
2026-03-21 15:54:04 -0700

    created an online friends list

--- client/src/components/OnlineFriendsList.vue
+++ client/src/components/OnlineFriendsList.vue
@@ -1,19 +1,69 @@
 <template>
   <v-card-title class="font-weight-bold">Who's Online?</v-card-title>
-  <v-card-text>
-    <v-list>
+  <v-card-text class="pa-0">
+    <v-list density="compact">
       <v-list-item v-for="friend in onlineFriends" :key="friend.id">
-        <v-list-item-avatar>
-          <v-img :src="friend.picture"></v-img>
-        </v-list-item-avatar>
-        <v-list-item-content>
-          <v-list-item-title>{{ friend.name }}</v-list-item-title>
-        </v-list-item-content>
+        <template v-slot:prepend>
+          <v-badge color="success" dot location="bottom end">
+            <v-avatar color="primary" size="32">
+              <v-img :src="friend.picture"></v-img>
+            </v-avatar>
+          </v-badge>
+        </template>
+        <v-list-item-title>{{ friend.username }}</v-list-item-title>
+      </v-list-item>
+      <v-list-item v-for="friend in offlineFriends" :key="friend.id">
+        <template v-slot:prepend>
+          <v-badge color="grey" dot location="bottom end">
+            <v-avatar color="primary" size="32">
+              <v-img :src="friend.picture"></v-img>
+            </v-avatar>
+          </v-badge>
+        </template>
+        <v-list-item-title class="text-grey">{{ friend.username }}</v-list-item-title>
       </v-list-item>
     </v-list>
   </v-card-text>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, computed, onMounted } from "vue";
+import { useFriendsStore } from "../stores/friendsStore.js";
+
+const allFriends = ref([]);
+const friendsStore = useFriendsStore();
+
+const onlineFriends = computed(() => {
+  return allFriends.value.filter((friend) =>
+    friendsStore.isFriendOnline(friend.id),
+  );
+});
+
+const offlineFriends = computed(() => {
+  return allFriends.value.filter(
+    (friend) => !friendsStore.isFriendOnline(friend.id),
+  );
+});
+
+const getFriends = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/api/friends/list", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+
+    allFriends.value = await response.json();
+  } catch (error) {
+    console.error("Error fetching friends:", error);
+  }
+};
+
+onMounted(() => {
+  getFriends();
+});
+</script>
 
 <style scoped></style>
